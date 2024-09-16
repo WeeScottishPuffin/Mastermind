dc72a8504392b7f74819784ac0ee5dbdc8ce48ef
Naut van der Winden <supposedbrit@gmail.com>
2024-09-16 13:33:59 +0200

    Updated colours to increase contrast between blue and cyan

--- Mastermind/colours.java
+++ Mastermind/colours.java
@@ -7,12 +7,12 @@ public class colours {
     static HashMap<String, String> _colourMap = new HashMap<>(6);
 
     public static HashMap<String, String> colourMap() {
-        _colourMap.put("r", "\u001B[31mo\u001B[0m");
-        _colourMap.put("y", "\u001B[33mo\u001B[0m");
-        _colourMap.put("g", "\u001B[32mo\u001B[0m");
-        _colourMap.put("b", "\u001B[34mo\u001B[0m");
-        _colourMap.put("m", "\u001B[35mo\u001B[0m");
-        _colourMap.put("c", "\u001B[36mo\u001B[0m");
+        _colourMap.put("r", "\u001B[1m\u001B[31mo\u001B[0m");
+        _colourMap.put("y", "\u001B[1m\u001B[33mo\u001B[0m");
+        _colourMap.put("g", "\u001B[1m\u001B[32mo\u001B[0m");
+        _colourMap.put("b", "\u001B[1m\u001B[34mo\u001B[0m");
+        _colourMap.put("m", "\u001B[1m\u001B[35mo\u001B[0m");
+        _colourMap.put("c", "\u001B[1m\u001B[36mo\u001B[0m");
         return _colourMap;
     }
 }
